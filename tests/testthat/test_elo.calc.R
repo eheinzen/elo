3b1e4851d01869b81e818996c058541b60d4dbe6
thebigez8 <thebigez8@gmail.com>
2017-08-05 11:18:14 -0500

    Tweak tests accordingly

--- tests/testthat/test_elo.calc.R
+++ tests/testthat/test_elo.calc.R
@@ -8,7 +8,7 @@ df <- data.frame(
   wins.A = c(1, 1, 0),
   dummy.A = 1500,
   dummy.B = 1500,
-  k = 20
+  k.column = 20
 )
 
 init <- c("Team A" = 1600, "Team B" = 1500, "Team C" = 1400)
@@ -19,22 +19,26 @@ init <- c("Team A" = 1600, "Team B" = 1500, "Team C" = 1400)
 
 test_that("Basic Elo calculations work", {
   expect_identical(
-    round(elo.run(wins.A ~ team.A + team.B, k = k, data = df)$elos[4, ], 3),
+    elo.run(wins.A ~ team.A + team.B + k(k.column), data = df)$elos,
+    elo.run(wins.A ~ team.A + team.B, k = 20, data = df)$elos
+  )
+  expect_identical(
+    round(elo.run(wins.A ~ team.A + team.B, k = 20, data = df)$elos[4, ], 3),
     c("Team A" = 1519.712, "Team B" = 1500.008, "Team C" = 1480.279)
   )
 
   expect_identical(
-    round(elo.run(wins.A ~ team.A + dummy.B, k = k, data = df)$elos[4, ], 3),
+    round(elo.run(wins.A ~ team.A + dummy.B + k(k.column), data = df)$elos[4, ], 3),
     c("Team A" = 1519.712, "Team C" = 1490)
   )
 
   expect_identical(
-    round(elo.run(wins.A ~ dummy.B + team.B, k = k, data = df)$elos[4, ], 3),
+    round(elo.run(wins.A ~ dummy.B + team.B, k = 20, data = df)$elos[4, ], 3),
     c("Team B" = 1500.288, "Team C" = 1490)
   )
 
   expect_identical(
-    suppressMessages(elo.run(wins.A ~ dummy.A + dummy.B, k = k, data = df)),
+    suppressWarnings(elo.run(wins.A ~ dummy.A + dummy.B, k = 20, data = df)),
     data.frame(elo.A = c(1510, 1510, 1490), elo.B = c(1490, 1490, 1510))
   )
 })
