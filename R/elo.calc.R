401564dcdf9c4f8182c6687cb344796c2dc11256
Ethan Heinzen <heinzen.ethan@mayo.edu>
2017-08-21 22:16:06 -0500

    Make elo.calc S3

---
+++ R/elo.calc.R
@@ -0,0 +1,48 @@
+
+#' Elo functions
+#'
+#' \code{elo.prob} calculates the probability that team A beats team B.
+#' \code{elo.update} calculates the update value for a given Elo matchup, and is used in
+#' \code{elo.calc}, which reports the post-update Elo values.
+#'
+#' These are all vectorized.
+#'
+#' @inheritParams elo.model.frame
+#' @param elo.A,elo.B Numeric vectors of elo scores.
+#' @param wins.A Numeric vector of wins by team A.
+#' @param k Numeric vector or scalar of k-values.
+#' @param ... Other arguments (not in use at this time).
+#' @details
+#'   Originally, I was going to script these in Rcpp, but the performance benefits are only realized
+#'   when the vectors get to be ~1000 elements long.
+#' @seealso \code{\link{elo.prob}}, \code{\link{elo.update}}
+#' @examples
+#' elo.calc(c(1500, 1500), c(1500, 1600), c(1, 0), k = 20)
+#' @name elo.calc
+NULL
+#> NULL
+
+#' @rdname elo.calc
+#' @export
+elo.calc <- function(elo.A, ...)
+{
+  UseMethod("elo.calc")
+}
+
+#' @rdname elo.calc
+#' @export
+elo.calc.default <- function(elo.A, elo.B, wins.A, k, ...)
+{
+  elo.up <- elo.update(elo.A = elo.A, elo.B = elo.B, wins.A = wins.A, k = k)
+  data.frame(elo.A = elo.A + elo.up, elo.B = elo.B - elo.up)
+}
+
+#' @rdname elo.calc
+#' @export
+elo.calc.formula <- function(formula, data, na.action, subset, ...)
+{
+  Call <- match.call()
+  Call[[1L]] <- quote(elo.model.frame)
+  Call$envir <- parent.frame()
+  mf <- eval(Call, parent.frame())
+}
