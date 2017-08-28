697b18a402e2df5f1749a153328ac910aa9c5b51
Ethan Heinzen <heinzen.ethan@mayo.edu>
2017-08-27 21:17:12 -0500

    Add helper-data script for tests

---
+++ tests/testthat/helper-data.R
@@ -0,0 +1,12 @@
+
+dat <- data.frame(
+  team.A = c("Team A", "Team A", "Team C"),
+  team.B = c("Team B", "Team C", "Team B"),
+  wins.A = c(1, 1, 0),
+  dummy.A = 1500,
+  dummy.B = 1500,
+  k.column = 20,
+  home.field = 10
+)
+
+init <- c("Team A" = 1600, "Team B" = 1500, "Team C" = 1400)
