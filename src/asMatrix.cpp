5e9bb1491af2563c9b5ab46b13f7a4809ba4b023
Ethan Heinzen <heinzen.ethan@mayo.edu>
2017-10-08 15:18:43 -0500

    Add asMatrix function for regressed elo objects

--- src/asMatrix.cpp
+++ src/asMatrix.cpp
@@ -2,11 +2,40 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-NumericMatrix eloRunAsMatrix(NumericMatrix mat, NumericVector initialElos, NumericMatrix regMat,
-                             LogicalVector regress, LogicalVector group)
+NumericMatrix eloRunAsMatrix(NumericMatrix mat, NumericVector initialElos, LogicalVector group)
 {
   // this function uses 1-based indexing, since the incoming matrix does, too
-  double nTeams = regMat.ncol();
+  double nTeams = initialElos.size();
+  double nGames = mat.nrow();
+  double nOut = sum(group);
+  NumericMatrix out(nOut, nTeams);
+  NumericVector curr(nTeams);
+  curr = initialElos;
+  int outRow = 0;
+
+  for(int i = 0; i < nGames; i++)
+  {
+    curr(mat(i, 0) - 1) = mat(i, 5);
+    if(mat(i, 1) > 0)
+    {
+      curr(mat(i, 1) - 1) = mat(i, 6);
+    }
+
+    if(group[i])
+    {
+      out(outRow, _) = curr;
+      outRow++;
+    }
+  }
+  return out;
+}
+
+// [[Rcpp::export]]
+NumericMatrix eloRunRegressedAsMatrix(NumericMatrix mat, NumericVector initialElos,
+                                      NumericMatrix regMat, LogicalVector regress, LogicalVector group)
+{
+  // this function uses 1-based indexing, since the incoming matrix does, too
+  double nTeams = initialElos.size();
   double nGames = mat.nrow();
   double nOut = sum(group);
   NumericMatrix out(nOut, nTeams);
@@ -36,3 +65,4 @@ NumericMatrix eloRunAsMatrix(NumericMatrix mat, NumericVector initialElos, Numer
   }
   return out;
 }
+
