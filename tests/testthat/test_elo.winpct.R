d702b11e215746a1622054e7574f43ce041b6d18
Ethan Heinzen <heinzen.ethan@mayo.edu>
2019-06-20 13:51:18 -0500

    Forgot to rename elo.winloss -> elo.winpct in the tests

--- tests/testthat/test_elo.winpct.R
+++ tests/testthat/test_elo.winpct.R
@@ -1,4 +1,4 @@
-context("Testing the elo.winloss function")
+context("Testing the elo.winpct function")
 
 ###########################################################################################################
 #### Do some simple checks
@@ -9,18 +9,18 @@ trn$diff <- score(trn$points.Home, trn$points.Visitor)
 trn <- trn[trn$diff %in% 0:1, ]
 trn$neut <- replace(rep(0, times = nrow(trn)), c(3, 30), 1)
 
-test_that("elo.winloss is working correctly", {
-  tmp.wl <- elo.winloss(score(points.Home, points.Visitor) ~ team.Home + team.Visitor, data = trn)
+test_that("elo.winpct is working correctly", {
+  tmp.wl <- elo.winpct(score(points.Home, points.Visitor) ~ team.Home + team.Visitor, data = trn)
 
-  expect_equal(tmp.wl$win.pct, c(10/13, 4/14, 7/12, 2/13, 7/13, 9/14, 6/10, 6/13))
+  expect_equal(unname(tmp.wl$win.pct), c(10/13, 4/14, 7/12, 2/13, 7/13, 9/14, 6/10, 6/13))
   expect_equal(sum(tmp.wl$win.pct * tmp.wl$n.games), 51)
   expect_equal(tmp.wl$n.games, as.vector(table(c(trn$team.Home, trn$team.Visitor))))
 
-  tmp.wl.wt <- elo.winloss(score(points.Home, points.Visitor) ~ team.Home + team.Visitor, data = trn, weights = 1:51)
+  tmp.wl.wt <- elo.winpct(score(points.Home, points.Visitor) ~ team.Home + team.Visitor, data = trn, weights = 1:51)
   expect_equal(sum(tmp.wl.wt$win.pct * tmp.wl.wt$n.games), sum(1:51))
   expect_equal(tmp.wl.wt$n.games, as.vector(xtabs(c(1:51, 1:51) ~ c(trn$team.Home, trn$team.Visitor))))
 
-  tmp.wl.players <- elo.winloss(score(points.Home, points.Visitor) ~ players(team.Home, "Zealous Zebras") + players(team.Visitor, "Yelling Yaks"), data = trn)
+  tmp.wl.players <- elo.winpct(score(points.Home, points.Visitor) ~ players(team.Home, "Zealous Zebras") + players(team.Visitor, "Yelling Yaks"), data = trn)
   expect_equal(sum(tmp.wl.players$win.pct * tmp.wl.players$n.games), 51)
   expect_equal(tmp.wl.players$n.games, 0.5*as.vector(table(c(trn$team.Home, trn$team.Visitor, rep(c("Zealous Zebras", "Yelling Yaks"), each = 51)))))
 })
