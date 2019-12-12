53fa1a661c2276de465bca25906a934f484dd3ab
Ethan Heinzen <heinzen.ethan@mayo.edu>
2019-12-12 10:09:20 -0600

    Add more tests

--- tests/testthat/test_elo.run.R
+++ tests/testthat/test_elo.run.R
@@ -67,7 +67,7 @@ results2 <- elo.run2(wins.A ~ adjust(team.A, 10) + team.B, data = dat, k = 20)
 
 test_that("prediction works correctly", {
   newdat <- data.frame(team.A = "Team A", team.B = "Team B")
-  expect_identical(
+  expect_ident(
     predict(results, newdata = newdat),
     elo.prob(final.elos(results)["Team A"], final.elos(results)["Team B"], adjust.A = 10),
     predict(results2, newdata = newdat),
@@ -83,7 +83,7 @@ test_that("Deep copying (#25)", {
 })
 
 test_that("Multiple k's (#45)", {
-  expect_identical(
+  expect_ident(
     elo.run(wins.A ~ adjust(team.A, 10) + team.B + k(k.column), data = dat)$elos,
     elo.run(wins.A ~ adjust(team.A, 10) + team.B + k(k.column, k.column), data = dat)$elos,
     elo.run2(wins.A ~ adjust(team.A, 10) + team.B + k(k.column), data = dat)$elos,
@@ -92,11 +92,38 @@ test_that("Multiple k's (#45)", {
 
   results <- elo.run(wins.A ~ adjust(team.A, 10) + team.B + k(k.column, 2*k.column), data = dat)
   results2 <- elo.run2(wins.A ~ adjust(team.A, 10) + team.B + k(k.column, 2*k.column), data = dat)
-  expect_identical(
+  expect_ident(
     rnd.mat(results, 3),
     rnd.mat(results2, 3),
     c("Team A" = 1519.145, "Team B" = 1501.183, "Team C" = 1470.830)
   )
 })
 
+test_that("Custom updates (#47)", {
+  custom_fun <- function(wins.A, elo.A, elo.B, k, adjust.A, adjust.B, ...)
+  {
+    wins.A - elo.prob(elo.A, elo.B, adjust.A = adjust.A, adjust.B = adjust.B)
+  }
+  expect_ident(
+    elo.run2(wins.A ~ adjust(team.A, 10) + team.B + k(k.column), data = dat, update.fun = custom_fun)$elos,
+    elo.run2(wins.A ~ adjust(team.A, 10) + team.B, k = 20, data = dat, update.fun = custom_fun)$elos,
+    elo.run2(wins.A ~ adjust(team.A, 10) + team.B, k = 1, data = dat)$elos
+  )
+  custom_fun2 <- function(wins.A, elo.A, elo.B, k, adjust.A, adjust.B, ...)
+  {
+    k*(wins.A - elo.prob(elo.A, elo.B, adjust.A = ifelse(elo.A > 1500, adjust.A / 2, adjust.A), adjust.B = adjust.B))
+  }
+  expect_identical(
+    elo.run2(wins.A ~ adjust(team.A, 10) + team.B, k = 20, data = dat, update.fun = custom_fun2)$elos[, -3],
+    elo.run2(wins.A ~ adjust(team.A, c(10, 5, 10)) + team.B, k = 20, data = dat)$elos[, -3]
+  )
+  custom_prob <- function(elo.A, elo.B, adjust.A, adjust.B)
+  {
+    unname(1/(1 + 10^(((elo.B + adjust.B) - (elo.A + ifelse(elo.A > 1500, adjust.A / 2, adjust.A)))/400.0)))
+  }
+  expect_identical(
+    elo.run2(wins.A ~ adjust(team.A, 10) + team.B, k = 20, data = dat, prob.fun = custom_prob, update.fun = custom_fun2)$elos,
+    elo.run2(wins.A ~ adjust(team.A, c(10, 5, 10)) + team.B, k = 20, data = dat)$elos
+  )
+})
 
